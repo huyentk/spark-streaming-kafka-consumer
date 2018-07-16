187e47d65c25c25210192e213e8dae15854e072d
huyentk <huyentk1296@gmail.com>
2018-07-16 14:46:34 +0700

    done

--- src/main/scala/com/cahac/spark/streaming/Database.scala
+++ src/main/scala/com/cahac/spark/streaming/Database.scala
@@ -1,49 +1,44 @@
-package com.cahac.spark.streaming
-
-import com.ibm.couchdb._
-import scalaz._
-import scalaz.concurrent.Task
-import org.slf4j.LoggerFactory
-
-object Database extends App {
-  private val logger = LoggerFactory.getLogger(Database.getClass)
-
-  // Define a simple case class to represent our data model
-  case class Person(name: String, age: Int)
-
-  // Define a type mapping used to transform class names into the doc kind
-  val typeMapping = TypeMapping(classOf[Person] -> "Person")
-  // Define some sample data
-  val alice = Person("Alice", 25)
-  val bob = Person("Bob", 30)
-  val carl = Person("Carl", 20)
-
-  // Create a CouchDB client instance
-  val couch = CouchDb("127.0.0.1", 5984, https = false, "huyentk", "Huyen1312")
-  // Define a database name
-  val dbName = "scala"
-  // Get an instance of the DB API by name and type mapping
-  val db = couch.db(dbName, typeMapping)
-
-  typeMapping.get(classOf[Person]).foreach { mType =>
-    val actions: Task[Seq[Person]] = for {
-      // Delete the database or ignore the error if it doesn't exist
-      _ <- couch.dbs.delete(dbName).ignoreError
-      // Create a new database
-      _ <- couch.dbs.create(dbName)
-      // Insert documents into the database
-      _ <- db.docs.create(alice)
-      // Retrieve all documents from the database and unserialize to Person
-      docs <- db.docs.getMany.includeDocs[Person].byTypeUsingTemporaryView(mType).build.query
-    } yield docs.getDocsData
-
-    // Execute the actions and process the result
-    actions.unsafePerformSyncAttempt match {
-      // In case of an error (left side of Either), print it
-      case -\/(e) => logger.error(e.getMessage, e)
-      // In case of a success (right side of Either), print each object
-      case \/-(a) => a.foreach(x => logger.info(x.toString))
-    }
-    couch.client.client.shutdownNow()
-  }
-}
+//package com.cahac.spark.streaming
+//
+//import com.ibm.couchdb._
+//import scalaz._
+//import scalaz.concurrent.Task
+//import org.slf4j.LoggerFactory
+//
+//object Database extends App {
+//  private val logger = LoggerFactory.getLogger(Database.getClass)
+//
+//  // Define a simple case class to represent our data model
+//  case class Person(name: String, age: Int)
+//
+//  // Define a type mapping used to transform class names into the doc kind
+//  val typeMapping = TypeMapping(classOf[Person] -> "Person")
+//  // Define some sample data
+//  val alice = Person("Alice", 25)
+//  val bob = Person("Bob", 30)
+//  val carl = Person("Carl", 20)
+//
+//  // Create a CouchDB client instance
+//  val couch = CouchDb("127.0.0.1", 5984, https = false, "huyentk", "Huyen1312")
+//  // Define a database name
+//  val dbName = "scala"
+//  // Get an instance of the DB API by name and type mapping
+//  val db = couch.db(dbName, typeMapping)
+//
+//  val actions = for {
+//    _ <- couch.dbs.delete(dbName).ignoreError
+//    _ <- couch.dbs.create(dbName)
+//    _ <- db.docs.create(alice)
+//    docs <- db.docs.getMany.includeDocs[Person].build.query
+//  } yield docs.getDocsData
+//
+//  // Execute the actions and process the result
+//  actions.unsafePerformSyncAttempt match {
+//    // In case of an error (left side of Either), print it
+//    case -\/(e) => logger.error(e.getMessage, e)
+//    // In case of a success (right side of Either), print each object
+//    case \/-(a) => a.foreach(x => logger.info(x.toString))
+//  }
+//  couch.client.client.shutdownNow()
+//
+//}
--- src/main/scala/com/cahac/spark/streaming/StreamingConsumer.scala
+++ src/main/scala/com/cahac/spark/streaming/StreamingConsumer.scala
@@ -1,30 +1,19 @@
 package com.cahac.spark.streaming
 
-import com.ibm.couchdb._
+import scalaj.http.{Http, HttpOptions}
+import org.json4s.DefaultFormats
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 import org.apache.spark.streaming.kafka010.KafkaUtils
-import org.slf4j.LoggerFactory
-import scalaz._
-import scalaz.concurrent.Task
-
 object StreamingConsumer {
+  implicit val formats: DefaultFormats.type = DefaultFormats
 
   case class Behavior(user: String, item: String)
 
   def main(args: Array[String]): Unit = {
-    val logger = LoggerFactory.getLogger(Database.getClass)
-    val typeMapping = TypeMapping(classOf[Behavior] -> "user_behavior")
-
-//    val alice = Person("Alice", 25)
-
-    val couch = CouchDb("127.0.0.1", 5984, https = false, "huyentk", "Huyen1312")
-    val dbName = "advertisement"
-    val db = couch.db(dbName, typeMapping)
-
-    val streamingContext = new StreamingContext(SparkCommon.conf, Seconds(30))
+    val streamingContext = new StreamingContext(SparkCommon.conf, Seconds(10))
 
     val kafkaParams = Map[String, Object](
       "bootstrap.servers" -> "localhost:9092",
@@ -34,7 +23,7 @@ object StreamingConsumer {
       "auto.offset.reset" -> "latest",
       "enable.auto.commit" -> (false: java.lang.Boolean)
     )
-    val topics = Array("welcome-message")
+    val topics = Array("behavior")
 
     // Create a input direct stream
     val stream = KafkaUtils.createDirectStream[String, String](
@@ -42,34 +31,21 @@ object StreamingConsumer {
       PreferConsistent,
       Subscribe[String, String](topics, kafkaParams)
     )
-    /*----Code for process received data here---*/
-    stream.foreachRDD(
-      rdd => {
-        if(!rdd.isEmpty()){
-          rdd.foreach(o => {
-            println(o)
-//            typeMapping.get(classOf[Behavior]).foreach { mType =>
-//              val actions: Task[Behavior] = for {
-//                // Insert documents into the database
-//                _ <- db.docs.create(Behavior())
-//                //            // Retrieve all documents from the database and unserialize to Person
-//                docs <- db.docs.getMany.includeDocs[Person].byTypeUsingTemporaryView(mType).build.query
-//              } yield docs.getDocsData
-//
-//              // Execute the actions and process the result
-//              actions.unsafePerformSyncAttempt match {
-//                // In case of an error (left side of Either), print it
-//                case -\/(e) => logger.error(e.getMessage, e)
-//                // In case of a success (right side of Either), print each object
-//                case \/-(a) => a.foreach(x => logger.info(x.toString))
-//              }
-//            }
-          })
-        }
+    //      {"item":"e34b237d-f014-4be8-a250-47c0bdb6d906","user":1, "type":"Behavior"}
+    stream.foreachRDD(rdd => {
+      if (!rdd.isEmpty()) {
+        rdd.foreach(o => {
+          val result = Http("http://127.0.0.1:5984/scala").postData(o.value())
+            .header("Content-Type", "application/json")
+            .header("Charset", "UTF-8")
+            .option(HttpOptions.readTimeout(10000)).asString
+          println(result.code)
+          println(result.body)
+        })
       }
-    )
+    })
+
     streamingContext.start()
     streamingContext.awaitTermination()
-    couch.client.client.shutdownNow()
   }
 }
